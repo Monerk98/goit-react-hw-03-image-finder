16fd08661811aa95587267d9567d8d4e8a7b93be
Monerk98 <81251116+Monerk98@users.noreply.github.com>
2021-12-14 10:36:25 +0200

    update
    update

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,7 +1,7 @@
-export default function fetchImg(searchName, pages) {
+export default function fetchImg(searchName, page) {
   const KEY = `23907146-6bbb396b18ee8800d3622a6a4`;
   return fetch(
-    `https://pixabay.com/api/?q=${searchName}&page=${pages}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    `https://pixabay.com/api/?q=${searchName}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
   ).then((res) => {
     if (res.ok) {
       return res.json();
